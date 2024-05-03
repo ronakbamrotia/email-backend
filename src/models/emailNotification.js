f46450f077db1f02c6e1fffab88fbbbbae939cdc
ronakbamrotia <bamrotiaronak@gmail.com>
2024-05-03 09:09:20 +0530

    added table structure as a comment

--- src/models/emailNotification.js
+++ src/models/emailNotification.js
@@ -3,6 +3,23 @@ const sequelize = require("../config/database");
 
 // Skipped the table migration part for now which can be done using sequelize-cli
 // Created the table structure manually in the database
+/*
+CREATE TABLE IF NOT EXISTS "email_notification" (
+    "id"  SERIAL, 
+    "uuid" UUID,
+    "key" VARCHAR(255) NOT NULL,
+    "subject" VARCHAR(255) NOT NULL,
+    "delayed_send" TIMESTAMP WITH TIME ZONE,
+    "body_data" JSON NOT NULL,
+    "email" JSON NOT NULL,
+    "bcc" JSON NOT NULL,
+    "body" TEXT NOT NULL, 
+    "status" VARCHAR(20) DEFAULT 'Pending', 
+    "created_at" TIMESTAMP WITH TIME ZONE NOT NULL,
+    "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL,
+    PRIMARY KEY ("id")
+);
+*/
 
 // Defining the database table schema 
 const emailNotification = sequelize.define("email_notification", {
